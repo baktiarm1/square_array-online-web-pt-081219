2d5d411143908459cae1d8f8799639fc94bf5867
Baktiar Miah <baktiar.m@gmail.com>
2019-08-21 18:45:05 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -3,5 +3,5 @@ def square_array(array)
   return num**2
 end
 
-brothers = ["Tim", "Tom", "Jim"]
-brothers.each{|brother| puts "Stop hitting yourself #{brother}!"}
+# brothers = ["Tim", "Tom", "Jim"]
+# brothers.each{|brother| puts "Stop hitting yourself #{brother}!"}
